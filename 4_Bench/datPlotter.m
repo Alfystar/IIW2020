96784a384662767aeed9ce0af785db0020503dea
fil-bad <filibad25051997@gmail.com>
2020-09-22 16:34:34 +0200

    added MATLAB script for graphs

--- 4_Bench/datPlotter.m
+++ 4_Bench/datPlotter.m
@@ -1,24 +1,35 @@
 % clc
 % close all
 % clear variables
-
-% per simulare una chiamata dal main
-%retCode = datPlotter("./benchMark/test/indexBenchMark.dat");
+% 
+% retCode = datPlotter_main("./elencoDat.txt");
+% 
+% % per simulare una chiamata dal main, decommentare le righe sopra
+% % e rinominare quella sottostante con il nome del file
+% 
 
 function out = datPlotter(argv)
 
-% if (argc ~= 1)
-%     print("Wrong format! use: @bash$ {PRGNAME} {PATHof.dat}\n");
-% end
+filesTab = readtable(argv, 'Delimiter', '\n');
 
-datValue = retrieveData(argv);
+for i = 1:height(filesTab)
+    file = char(filesTab.(1)(i));
+    analizeDatFile(file);
+end
+out = 0;
+
+end
+
+function sfile = analizeDatFile(file)
+
+datValue = retrieveData(file);
 
 for i = 2+1:size(datValue, 2)
     fig = printGraph(datValue(:, [1 2 i]), i);
     
-    k = strfind(argv, '/');
+    k = strfind(file, '/');
     
-    folder = extractBefore(argv, k(end)); %% use last '/' to get path
+    folder = extractBefore(file, k(end)); %% use last '/' to get path
     
     mkdir(folder + "/imgs");
     
@@ -29,13 +40,12 @@ for i = 2+1:size(datValue, 2)
     saveas(fig, path, 'png');
 end
 
-out = 0;
+sfile = 0;
 end
 
 function tab = retrieveData(path)
 % to retrieve numbers data
-num = readtable(path, 'VariableNamingRule','preserve');
-tab = num;
+tab = readtable(path, 'VariableNamingRule','preserve');
 
 end
 
@@ -68,7 +78,7 @@ while(nCore > 0)
     subDat = subDat(1:indexes(1)-1, :); % we reduce size of subDat for the next iteration of find
     
     plot(valueTab(:,1), valueTab(:,2), '-*', ...
-          'DisplayName', PlotName + ": " + nCore);
+        'DisplayName', PlotName + ": " + nCore);
     grid on
     hold on
     
@@ -76,7 +86,7 @@ while(nCore > 0)
         break;
     end
     
-    nCore = nCore - 1; 
+    nCore = nCore - 1;
 end
 
 % analyzed all data, adding some info and save on file
@@ -84,14 +94,14 @@ end
 hold off
 
 
-xlabel(XName, 'Interpreter', 'none');
-ylabel(YName, 'Interpreter', 'none');
+xlabel(XName)%, 'Interpreter', 'none');
+ylabel(YName)%, 'Interpreter', 'none');
 
 %legend('Interpreter','none', 'Location', 'best');
 
 if(valueTab(1,2) < valueTab(end,2)) %% the graph is increasing
     position = 'Northwest';
-else 
+else
     position = 'Northeast';
 end
 
